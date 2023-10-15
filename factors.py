0101d7e85bbfdd856f29fa61dd0248b5620d02c7
Kristin Evans <kristinlashun@gmail.com>
2023-10-14 21:58:05 -0700

    space added

--- factors.py
+++ factors.py
@@ -1,4 +1,4 @@
-user_input = int(input("Please enter a positive integer:"))
+user_input = int(input("Please enter a positive integer: "))
 print("The factors of", user_input, "are:")
 for i in range(1, user_input + 1):
     if user_input % i == 0:
