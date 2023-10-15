f3edf652efb22270e3237979dd8275309328db8d
Kristin Evans <kristinlashun@gmail.com>
2023-10-14 21:54:52 -0700

    input on same line

--- factors.py
+++ factors.py
@@ -1,5 +1,4 @@
-print("Please enter a positive integer:")
-user_input = int(input())
+user_input = int(input("Please enter a positive integer:"))
 print("The factors of", user_input, "are:")
 for i in range(1, user_input + 1):
     if user_input % i == 0:
